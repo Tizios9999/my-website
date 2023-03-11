d485034f62a39beab6717045b5b1921145e00c26
Tizios9999 <davide.santonocito@outlook.com>
2023-03-11 16:50:28 +0100

    styled about me

--- components/About/About.js
+++ components/About/About.js
@@ -3,10 +3,12 @@ import styles from "./About.module.scss"
  
 const About = ({ blok }) => {
   return <div {...storyblokEditable(blok)} className={styles["container"]}>
+          <div className={styles["neon-border"]}>
             <h1>About me</h1>
             <h2>{blok.title}</h2>
             <p>{blok.description}</p>
-        </div>;
+          </div>
+        </div>
 };
  
 export default About;
