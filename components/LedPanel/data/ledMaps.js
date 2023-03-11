f57d730300776378f0615adea59bf9ca674fcbf3
Tizios9999 <davide.santonocito@outlook.com>
2023-03-11 15:32:41 +0100

    created ledMaps file to map panels lights animations

---
+++ components/LedPanel/data/ledMaps.js
@@ -0,0 +1,21 @@
+
+
+
+
+const menuButton = [5, 5, 5, 5, 5];
+
+const menuTransition = [1, 3, 1,
+                        1, 3, 2,
+                           3, 2,
+                           3, 1,
+                        1, 3, 1]
+
+const closeButton =    [1,    3,    1, 
+                        1, 1, 1, 1, 3,
+                                 1, 3,
+                           1, 1, 1, 1,
+                        1,    3,    1];
+
+const ledPanelAnimationMap = [menuButton, menuTransition, closeButton]; 
+
+export default ledPanelAnimationMap;
