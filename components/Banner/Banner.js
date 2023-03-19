a765747afc5de561c879bc1424a20092a9de3a82
Tizios9999 <davide.santonocito@outlook.com>
2023-03-19 14:19:06 +0100

    add: installed fonts locally

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,14 +1,26 @@
 import { storyblokEditable } from "@storyblok/react";
 import styles from './Banner.module.scss';
 import classNames from 'classnames';
+import { useRef } from 'react';
 
 const Banner = ({ blok }) => {
 
   const borderClasses = classNames(styles["neon-window"], styles["neon-border-lit"]);
+  const borderRef = useRef();
+
+  // const handleClick = () => {
+  //   borderRef.current.classList.remove('.neon-border-lit');
+
+  //   console.log("AAAAAAA!")
+
+  //   window.getComputedStyle(borderRef.current).getPropertyValue('border');
+
+  //   borderRef.current.classList.add('.neon-border-off');
+  // }
 
   return ( 
-        <div className={styles["section-container"]}>
-        <section className={borderClasses}>
+        <div className={styles["section-container"]} >
+        <section ref={borderRef} className={borderClasses}>
           <div {...storyblokEditable(blok)} className={styles["blok-container"]}>
               <h2 className={styles["intro"]}>{blok.intro}</h2>
               <h1 className={styles["my-name"]}>{blok.name}</h1>
