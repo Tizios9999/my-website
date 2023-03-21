7e5feac7b388413773f2be3f5d31149810f82345
Tizios9999 <davide.santonocito@outlook.com>
2023-03-21 14:32:18 +0100

    slider update

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -2,32 +2,41 @@ import { storyblokEditable } from "@storyblok/react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './Slider.module.scss';
 import 'swiper/css';
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
  
 const Slider = ({ blok }) => {
 
   return  <section className={styles["section-container"]}>
           <div className={styles["blok-container"]} {...storyblokEditable(blok)}>
-          <h2>Projects</h2>
-          <div className={styles["slider-container"]}>
-            <div className={styles["card-wrapper"]}>
-
+          <h2>Most relevant Projects</h2>
+          <div className={styles["swiper-container"]}>
             
           <Swiper
+            navigation={true}
+            modules={[Navigation]}
             spaceBetween={50}
             slidesPerView={1}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
+            className={styles["sw-el"]}
           >
             {console.log(blok.cards, "Cards")}
             {blok.cards.map((card) => {
               return (
                 <SwiperSlide className={styles["card-container"]}>
-                <div>
-                    <h3>{card.title}</h3>
+                <div className={styles["custom-slide-container"]}>
+                    <h3 className={styles["project-title"]}>{card.title}</h3>
                     <div className={styles["img-wrapper"]}>
                         <img src={card.image.filename}></img>
                     </div>
                     <div className={styles["desc"]}><p>{card.description}</p></div>
+                    <div className={styles["technologies-desc"]}><p>TECHNOLOGIES HERE</p></div>
+                    <div className={styles["project-buttons-wrapper"]}>
+                      <button>Live</button>
+                      <button>Github</button>
+                      <button>Blog</button>
+                    </div>
                 </div>
                 </SwiperSlide>
 
@@ -37,7 +46,6 @@ const Slider = ({ blok }) => {
             </Swiper>
             </div>
         </div>
-    </div>
     </section>
 };
  
