2d13f5ad4865d453d588e2267013c89e5d720974
Tizios9999 <davide.santonocito@outlook.com>
2023-04-20 11:32:47 +0200

    chore: img alt correction

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -9,11 +9,11 @@ const Footer = () => {
                 <p>Don't hesitate to contact me for any idea you might have in mind!</p>
                 <p>You can find me on my LinkedIn here below</p>
                 <a href="https://www.linkedin.com/in/davide-santonocito-36ab84170/" target="_blank">
-                <img className={styles["icon"]} src="./linkedin-icon.svg" alt="Project Logo"></img>
+                <img className={styles["icon"]} src="./linkedin-icon.svg" alt="Linkedin Icon"></img>
                 </a>
                 <p>...and here below is my Github profile.</p>
                 <a href="https://github.com/Tizios9999" target="_blank">
-                <img className={styles["icon"]} src="./github-icon.svg" alt="Project Logo"></img>
+                <img className={styles["icon"]} src="./github-icon.svg" alt="Github Link"></img>
                 </a>
                 <p>See you soon! 😃</p>
             </div>
