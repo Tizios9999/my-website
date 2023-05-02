efb1c1b9022eaab94235e20a60123ca06e7151bc
Aurora <aurora.grippaudo@dotenv.it>
2023-05-02 14:07:57 +0200

    fixed

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -73,7 +73,7 @@ const Navbar = () =>
             <li onClick={() => changeSection(0)}>Home</li>
             <li onClick={() => changeSection(1)}>About Me</li>
             <li onClick={() => changeSection(2)}>My Projects</li>
-            <li><a href="./blog">Blog</a></li>
+            <li><a href="/blog">Blog</a></li>
             <li onClick={() => {scrollToSection('contact'), dispatch({ type: "TOGGLE_HAMBURGER_STATUS"})}}>Contact Me</li>
         </ul>
     </div>
